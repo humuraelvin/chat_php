c376ff62510abbc706ddc555c25f2221dcb6f9ce
HUMURA ELVIN <humura53@gmail.com>
2024-05-22 21:44:09 +0200

    changes made

--- php/users.php
+++ php/users.php
@@ -1,27 +1,14 @@
 <?php
-
-session_start();
-
-include_once "config.php"; 
-$sql = mysqli_query($conn, "SELECT * FROM users");
-$output = "";
-
-if (mysqli_num_rows($sql) == 1) {
-    $output .= "No users are available to chat";
-} elseif (mysqli_num_rows($sql) > 0) {
-    while ($row = mysqli_fetch_assoc($sql)) {
-        $output .= '<a href="#">
-                    <div class="content">
-                    <img src="php/images/'.$row['img'].'" alt="" />
-                    <div class="details">
-                        <span>'.$row['fname']." ".$row['lname'].'</span>
-                        <p>This is a test message</p>
-                    </div>
-                    </div>
-                    <div class="status-dot"><i class="fas fa-circle"></i></div>
-                </a>';
+    session_start();
+    include_once "config.php";
+    $outgoing_id = $_SESSION['unique_id'];
+    $sql = "SELECT * FROM users WHERE NOT unique_id = {$outgoing_id} ORDER BY user_id DESC";
+    $query = mysqli_query($conn, $sql);
+    $output = "";
+    if(mysqli_num_rows($query) == 0){
+        $output .= "No users are available to chat";
+    }elseif(mysqli_num_rows($query) > 0){
+        include_once "data.php";
     }
-}
-echo $output;
-
+    echo $output;
 ?>
